cf4026a516b93a08f02b4af821a1768acaee093f
Mark Headd <mheadd@gmail.com>
2013-07-12 12:43:48 -0400

    Parser refinements and sample web socket page

--- api.js
+++ api.js
@@ -1,19 +1,36 @@
+/**
+ * TODO: Error handling / Logging.
+ * TODO: Forever!
+ */
+
 // Include required modules.
 var express = require('express');
 var socketio = require('socket.io');
 var redis = require("redis");
 
+// Port to run server on.
+var port = process.argv[2] || 3000;
+
 var app = express();
 
 // REST endpoint.
 app.get('/', function(req, res){
-  res.send('hello world');
+	res.sendfile(__dirname + '/index.html');
+});
+
+app.get('/assets/css/:file', function(req, res) {
+	res.sendfile(__dirname + '/assets/css/' + req.params.file);
+});
+
+app.get('/assets/js/:file', function(req, res) {
+	res.sendfile(__dirname + '/assets/js/' + req.params.file);
 });
 
 app.get('/:number', function(req, res){
 	var flightNumber = req.params.number;
 	var redisClient = redis.createClient();
 	redisClient.get(flightNumber, function(err, reply) {
+		res.set('Cache-Control', 'max-age=60');
 		if(req.query.callback) {
 			res.jsonp(200, reply);
 		}
@@ -25,13 +42,19 @@ app.get('/:number', function(req, res){
 });
 
 // WebSocket endpoint.
-var socket = require('socket.io').listen(app.listen(3000));
+var socket = require('socket.io').listen(app.listen(port),{log: false});
 
 socket.on('connection', function(client){
 
 	var redisClient = redis.createClient();
 
 	client.on('send', function(data) {
+
+		// If client is already subscribed, get a fresh connection
+		if(redisClient.connected) {
+			redisClient.quit();
+			redisClient = redis.createClient();
+		}		
 
 		// Respond with curret flight status.
 		redisClient.get(data, function(err, reply) {
--- parser.js
+++ parser.js
@@ -1,50 +1,65 @@
+/**
+ * TODO: Error handling / Logging.
+ * TODO: Forever!
+ */
+
 // Include required modules.
 var fs = require('fs');
 var redis = require("redis");
 
-// TODO: Change to fs.watch.
-fs.readFile('data/InfaxESB.dat.dat', function(err, data) {
-    if(err) throw err;
+// Watch the data file for changes.
+fs.watch(__dirname + '/data/InfaxESB.dat', function(event, filename) { 
+	if(event == 'change') {
+		fs.readFile(__dirname + '/data/InfaxESB.dat', function(err, data) {
 
-    // Split the file on line breaks to get each record.
-    var flightRecords = data.toString().split("\n");
+			//TODO: Error handling (log to syslog?)
+		    if(err) throw err;
 
-    // Firt row is the date/time of update
-    var updateDateTime = flightRecords[0].replace(/[\r]/g, '');
+		    // Split the file on line breaks to get each record.
+		    var flightRecords = data.toString().split("\n");
 
-    // Connect to Redis instance and remove records from previous update.
-    redisClient = redis.createClient();
-    redisClient.flushdb();
+		    // Firt row is the date/time of update
+		    var updateDateTime = (flightRecords[0].substring(0,8) + ' ' +  flightRecords[0].substring(8,10) + ':' + flightRecords[0].substring(10,12)).replace(/[\r]/g, '');
 
-    // Process flight records.
-    for(var i=1; i<flightRecords.length; i++) {
-    	if(flightRecords[i].length > 0) {
+		    // Connect to Redis instance and remove records from previous update.
+		    redisClient = redis.createClient();
+		    redisClient.flushdb();
 
-    		// Parse string containing flight information (it ain't pretty, but it gets the job done!)
-			var direction = flightRecords[i].substring(0,1).replace('D', 'Departure').replace('A', 'Arrival');
-	    	var flightType = flightRecords[i].substring(1,2).replace('I', 'International').replace('D', 'Domestic');
-	    	var airline = flightRecords[i].substring(2,4);
-	    	var flightNumber = flightRecords[i].substring(6,10).replace(/(^[\s]+|[\s]+$)/g, '');
-	    	var airport = flightRecords[i].substring(10,25).replace(/(^[\s]+|[\s]+$)/g, '');
-	    	var scheduledDateTime = flightRecords[i].substring(25,37);
-	    	var estimatedDateTime = flightRecords[i].substring(37,41);
-	    	var gate = flightRecords[i].substring(41,44);
-	    	var status = flightRecords[i].substring(44,54).replace(/(^[\s]+|[\s]+$)/g, '');
+		    // Utility function to trim whitespace.
+		    function trimSpaces(field) {
+		    	return field.replace(/(^[\s]+|[\s]+$)/g, '')
+		    }
 
-	    	// Create an object to hold flight details.
-	    	var flightObject = 
-	    		{'updated': updateDateTime, 'direction': direction, 'flightType': flightType, 'airline': airline, 
-	    		 'flightNumber': flightNumber, 'airport': airport, 'scheduledDateTime': scheduledDateTime, 
-	    		 'estimatedDateTime': estimatedDateTime, 'gate': gate, 'status': status
-	    		};
+		    // Process flight records.
+		    for(var i=1; i<flightRecords.length; i++) {
+		    	if(flightRecords[i].length > 0) {
 
-	    	// Update redis with new flight information (for REST calls).	 
-	        redisClient.set(flightNumber, JSON.stringify(flightObject));
+		    		// Parse string containing flight information.
+					var direction = flightRecords[i].substring(0,1).replace("D", "Departure").replace("A", "Arrival");
+			    	var flightType = flightRecords[i].substring(1,2).replace("I", "International").replace("D", "Domestic");
+			    	var airline = flightRecords[i].substring(2,4);
+			    	var flightNumber = trimSpaces(flightRecords[i].substring(6,10));
+			    	var airport = trimSpaces(flightRecords[i].substring(10,25));
+			    	var scheduledDateTime = flightRecords[i].substring(25,33) + ' ' + flightRecords[i].substring(33,35) + ':' + + flightRecords[i].substring(35,37);;
+			    	var estimatedTime = flightRecords[i].substring(37,39) + ':' + flightRecords[i].substring(39,41);
+			    	var gate = flightRecords[i].substring(41,44);
+			    	var status = trimSpaces(flightRecords[i].substring(44,55));
 
-	        // Publush updated flight information on channel matching flight number (for WebSocket connections).
-	        redisClient.publish(flightNumber, JSON.stringify(flightObject));
-    	}
-    }
-    redisClient.quit();
-    console.log("Done.\n");
+			    	// Create an object to hold flight details.
+			    	var flightObject = 
+			    		{'updated': updateDateTime, 'direction': direction, 'flightType': flightType, 'airline': airline, 
+			    		 'flightNumber': flightNumber, 'airport': airport, 'scheduledDateTime': scheduledDateTime, 
+			    		 'estimatedTime': estimatedTime, 'gate': gate, 'status': status
+			    		};
+
+			    	// Update redis with new flight information (for REST calls).	 
+			        redisClient.set(flightNumber, JSON.stringify(flightObject));
+
+			        // Publush updated flight information on channel matching flight number (for WebSocket connections).
+			        redisClient.publish(flightNumber, JSON.stringify(flightObject));
+		    	}
+		    }
+		    redisClient.quit();
+		});
+	}
 });
